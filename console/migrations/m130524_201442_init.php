fbd7e2b79bb3a6909099eef4491a89796e5ff3f4
webcoderphp <asadbekdev007@gmail.com>
2023-08-05 12:40:17 +0500

    Customize core migration files, add columns to user.

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -1,10 +1,10 @@
 <?php
 
-use yii\db\Migration;
+use common\components\migrations\CustomMigration;
 
-class m130524_201442_init extends Migration
+class m130524_201442_init extends CustomMigration
 {
-    public function up()
+    public function safeUp()
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
@@ -15,19 +15,28 @@ class m130524_201442_init extends Migration
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
+            'first_name' => $this->string(100)->notNull(),
+            'last_name' => $this->string(100)->notNull(),
+            'phone' => $this->string(100),
+            'address' => $this->string(250),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
+            'created_by' => $this->integer(11)->notNull(),
+            'updated_by' => $this->integer(11)->notNull(),
         ], $tableOptions);
+        $this->customAddForeignKey('user','created_by','user','id');
+        $this->customAddForeignKey('user','updated_by','user','id');
     }
 
-    public function down()
+    public function safeDown()
     {
+        $this->customDropForeignKey('user','updated_by');
+        $this->customDropForeignKey('user','created_by');
         $this->dropTable('{{%user}}');
     }
 }
--- console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -4,12 +4,12 @@ use \yii\db\Migration;
 
 class m190124_110200_add_verification_token_column_to_user_table extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->dropColumn('{{%user}}', 'verification_token');
     }
